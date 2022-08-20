4f3fc1d219204d27ee6630c58d3897638f06bae4
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-08-19 20:47:57 -0500

    writeFile Promise

--- index.js
+++ index.js
@@ -110,3 +110,24 @@ let promptQuestions = employeeData => {
       });
 
 }
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+      fs.writeFile('./dist/team_profile.html', fileContent, err => {
+        // if there's an error, turn down the Promise and send the error to the Promise's `.catch()` method
+        if (err) {
+          reject(err);
+          // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+          return;
+        }
+  
+        // if everything went well, settle the Promise and send the successful data to the `.then()` method
+        resolve({
+          ok: true,
+          message: 'File successfully created!'
+        });
+      });
+    });
+  };
+
+  
