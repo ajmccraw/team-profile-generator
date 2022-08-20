df9ed5b0ea01648117a7d7ca3883e62c9dcd6ff1
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-08-19 20:25:53 -0500

    need to add js file in src folder

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ const Employee = require('./lib/employee');
 const Engineer = require('./lib/engineer');
 const Manager = require('./lib/manager');
 const Intern = require('./lib/intern');
-const 
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateHTML = require('');
+
+let managers = [];
+let engineers = [];
+let interns = [];
